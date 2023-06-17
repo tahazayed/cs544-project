00a938a8fc284d96f07d1410d9276f3bb78392da
iqhossan <hossan.iqbal@gmail.com>
2023-06-17 11:33:00 -0500

    change folder structure

--- post-service/src/main/java/cs544/PostApplication.java
+++ post-service/src/main/java/cs544/PostApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class App {
+public class PostApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(PostApplication.class, args);
     }
 }
