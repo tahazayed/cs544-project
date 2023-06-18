3350417b7944f6191e4c7f1dd29986740dbf21de
Taha Elsayed <tahazayed@gmail.com>
2023-06-18 00:06:53 -0500

    fix user-service build error

--- gateway-service/src/main/java/cs544/controller/CommentRestController.java
+++ gateway-service/src/main/java/cs544/controller/CommentRestController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api")
 public class CommentRestController {
 
     private final ICommentService commentService;
