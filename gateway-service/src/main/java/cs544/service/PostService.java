32468dbcdf2cfbced528613146960a7ef2bbeeb2
Taha Elsayed <tahazayed@gmail.com>
2023-06-21 21:10:15 -0500

    update postman collection

--- gateway-service/src/main/java/cs544/service/PostService.java
+++ gateway-service/src/main/java/cs544/service/PostService.java
@@ -7,6 +7,7 @@ import cs544.dto.PostCreationObject;
 import cs544.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
